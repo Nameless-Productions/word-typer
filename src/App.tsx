da0a6c92823e8fcf53d3855a10cd4c8a034bd196
Nameless <nameless@thenamelessdev.com>
2026-04-21 22:02:57 +0200

    feat: add ability to view new words

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect, useRef, useState } from "react"
+import createWord from "./lib/createWord";
+
 export default function App() {
+  const [word, setWord] = useState("");
+  const cancelledRef = useRef(false)
+
+  const updateWord = async (cancelled: boolean) => {
+    const newWord = await createWord()
+    if(!newWord || cancelled) return
+    setWord(newWord)
+  }
+
+  useEffect(() => {
+    updateWord(cancelledRef.current)
+    return () => { cancelledRef.current = true }
+  }, [])
+
   return (<>
     <p className="font-bold text-xl">Word Typer</p>
+
+    <br />
+
+    <p>{word}</p>
   </>)
 }
